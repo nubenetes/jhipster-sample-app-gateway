158a608d999e51a3b166065595b7d2432cc4e115
Inaki <inafev@users.noreply.github.com>
2026-06-30 21:10:02 +0200

    fix: drop stray hibernate @Cache from reactive User entity (R2DBC app has no hibernate-core; fixes compile)

--- src/main/java/io/github/jhipster/sample/domain/User.java
+++ src/main/java/io/github/jhipster/sample/domain/User.java
@@ -13,8 +13,6 @@ import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
 import org.apache.commons.lang3.StringUtils;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
@@ -23,7 +21,6 @@ import org.springframework.data.relational.core.mapping.Table;
  * A user.
  */
 @Table("jhi_user")
-@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class User extends AbstractAuditingEntity<Long> implements Serializable {
 
     @Serial
